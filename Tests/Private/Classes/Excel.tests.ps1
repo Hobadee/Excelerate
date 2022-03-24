95fc9c3012e5502e511dfd4770a420ead39c4f72
Eric Kincl <eric@kincl.net>
2022-03-24 02:10:32 -0700

    Enhanced our unit testing

--- Tests/Private/Classes/Excel.tests.ps1
+++ Tests/Private/Classes/Excel.tests.ps1
@@ -9,11 +9,14 @@ InModuleScope Excelerate{
                 $xlDir = "Tests"
                 $fname = "blank.xlsx"
                 $workbookFile = "xl/workbook.xml"
+                $relsFile = "_rels/.rels"
+                $saveFile = "/Users/eric.kincl/src/excelerate/testExcel.xlsx"
                 $xl = (Join-Path $xlDir $fname)
                 $excel = [Excel]::new($xl)
             }
             AfterAll{
                 $excel.Dispose()
+                Remove-Item $saveFile
             }
 
 
@@ -24,9 +27,10 @@ InModuleScope Excelerate{
             It "Checking files exist"{
                 $excel.getTempDir() | Should -exist
                 (Join-Path $excel.getTempDir() $workbookFile ) | Should -exist
+                (Join-Path $excel.getTempDir() $relsFile ) | Should -exist
             }
 
-            
+
             It "Checking Getter Types"{
                 $excel.getTempDir() | Should -BeOfType string
                 $excel.getWorkbook() | Should -BeOfType xml
@@ -34,6 +38,19 @@ InModuleScope Excelerate{
             }
             It "Checking Getters"{
                 $excel.getFilename() | Should -Be $fname
+            }
+
+            It "Checking Save As operations"{
+                # Test that "saveAs" without a dir saves to current directory
+                # Test that "saveAs" with a dir saves to the correct directory
+                # Test that saving on an existing file does/doesn't overwrite it
+
+                $saved = $excel.saveAs($saveFile)
+                $saved | Should -BeOfType System.IO.FileInfo
+                $saved.FullName | Should -Be $saveFile
+            }
+            It "Checking Save As overwrite behavior"{
+                { $excel.saveAs($saveFile) } | Should -Throw
             }
         }		
 	}
