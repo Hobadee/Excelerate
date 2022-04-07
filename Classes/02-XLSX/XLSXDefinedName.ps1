295edf5dec59fe0a8ecd1d509a87ed4fa0e0e559
Eric Kincl <eric@kincl.net>
2022-04-06 22:16:20 -0700

    Initial stab at DefinedName classes
    ...including a method to remove basic broken refs.

--- Classes/02-XLSX/XLSXDefinedName.ps1
+++ Classes/02-XLSX/XLSXDefinedName.ps1
@@ -1,4 +1,46 @@
 class XLSXDefinedName{
 
+    [string]$name
+    [string]$value
+    [string]$localSheetId
+    [boolean]$hidden
+
+
+    <##################
+    ##### GETTERS #####
+    ##################>
+    [string]getName(){
+        return $this.name
+    }
+    [string]getValue(){
+        return $this.value
+    }
+    [string]getLocalSheetId(){
+        return $this.localSheetId
+    }
+    [boolean]getHidden(){
+        return $this.hidden
+    }
+
+
+    <##################
+    ##### SETTERS #####
+    ##################>
+    [XLSXDefinedName]setName([string]$name){
+        $this.name = $name
+        return $this
+    }
+    [XLSXDefinedName]setValue([string]$value){
+        $this.value = $value
+        return $this
+    }
+    [XLSXDefinedName]setLocalSheetId([string]$Id){
+        $this.localSheetId = $Id
+        return $this
+    }
+    [XLSXDefinedName]setHidden([boolean]$hidden){
+        $this.hidden = $hidden
+        return $this
+    }
 
 }
--- Classes/02-XLSX/XLSXDefinedNames.ps1
+++ Classes/02-XLSX/XLSXDefinedNames.ps1
@@ -1,4 +1,29 @@
 class XLSXDefinedNames : System.Collections.Generic.List[PSObject]{
 
 
+    <##
+     # Removes invalid references from this object
+     #
+     # NOTE: This will only remove references with the *EXACT* value of '#REF!'
+     # 
+     # NOTE: This is desctructive and will remove data!
+     #
+     # @return int Returns the number of references removed
+     #>
+    [int]removeBrokenRefs(){
+        [int]$removed = 0
+        foreach($XLSXDefinedName in $this){
+            if($XLSXDefinedName.getValue() -eq '#REF!'){
+                if($this.Remove($XLSXDefinedName)){
+                    $removed++
+                }
+                else{
+                    $str = "Failed to remove item: " + $XLSXDefinedName.getName()
+                    throw $str
+                }
+            }
+        }
+        return $removed
+    }
+
 }
