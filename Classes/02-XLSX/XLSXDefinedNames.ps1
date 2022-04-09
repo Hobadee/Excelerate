9019f5a5f4b274e2a6083c7cc46821f332cfdbad
Eric Kincl <eric@kincl.net>
2022-04-08 23:41:08 -0700

    Refactored the class
    -We can't do a search and remove in the same foreach() loop - it will error out.
    -This forces our code to be cleanerish - we must now do the search and removal separately
    -We now have a search method and a removal method.  They are easy to tie together, but will also work separately nicely.
    -Searches are now done via wildcard (`-like`) and thus catch more broken names

--- Classes/02-XLSX/XLSXDefinedNames.ps1
+++ Classes/02-XLSX/XLSXDefinedNames.ps1
@@ -2,25 +2,60 @@ class XLSXDefinedNames : System.Collections.Generic.List[PSObject]{
 
 
     <##
-     # Removes invalid references from this object
+     # Finds and Removes invalid references from this object
+     # An invalid reference is any reference that contains "#REF!"
      #
-     # NOTE: This will only remove references with the *EXACT* value of '#REF!'
+     # NOTE: This may take some time to run on large datasets
      # 
      # NOTE: This is desctructive and will remove data!
      #
      # @return int Returns the number of references removed
      #>
-    [int]removeBrokenRefs(){
+    [int]findRemoveBrokenRefs(){
+        $names = $this.findBrokenRefs()
+        $removed = $this.removeNamesByObject($names)
+        return $removed
+    }
+
+
+    <##
+     # Find invalid references in this object, and return a new object containing those references
+     # An invalid reference is any reference that contains "#REF!" ( -like "*#REF!*" )
+     #
+     # NOTE: This may take some time to run on large datasets
+     #
+     # @return XLSXDefinedNames Returns an XLSXDefinedNames object with a bunch of children XLSXDefinedName objects that have broken refs
+     #>
+    [XLSXDefinedNames]findBrokenRefs(){
+        $names = [XLSXDefinedNames]::new()
+        
+        foreach($item in $this){
+            if($item.getValue() -like '*#REF!*'){
+                $names.add($item)
+            }
+        }
+
+        return $names
+    }
+
+
+    <##
+     # Removes names from this XLSXDefinedNames object by passing them as a new XLSXDefinedNames object
+     #
+     # NOTE: This is desctructive and will remove data!
+     #
+     # @param XLSXDefinedNames $names Takes a XLSXDefinedNames list of XLSXDefinedName objects to remove from this object
+     # @return int Number of items removed from this object
+     #>
+    [int]removeNamesByObject([XLSXDefinedNames]$names){
         [int]$removed = 0
-        foreach($XLSXDefinedName in $this){
-            if($XLSXDefinedName.getValue() -eq '#REF!'){
-                if($this.Remove($XLSXDefinedName)){
-                    $removed++
-                }
-                else{
-                    $str = "Failed to remove item: " + $XLSXDefinedName.getName()
-                    throw $str
-                }
+        foreach($item in $names){
+            if($this.Remove($item)){
+                $removed++
+            }
+            else{
+                $str = "Failed to remove item: " + $item.getName()
+                throw $str
             }
         }
         return $removed
