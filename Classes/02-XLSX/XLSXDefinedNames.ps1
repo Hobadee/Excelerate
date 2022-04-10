ec51626306ed832c602569664b8d19c32bb06331
Eric Kincl <eric@kincl.net>
2022-04-10 00:08:26 -0700

    Added progress bars since this takes a while to run

--- Classes/02-XLSX/XLSXDefinedNames.ps1
+++ Classes/02-XLSX/XLSXDefinedNames.ps1
@@ -27,14 +27,24 @@ class XLSXDefinedNames : System.Collections.Generic.List[PSObject]{
      # @return XLSXDefinedNames Returns an XLSXDefinedNames object with a bunch of children XLSXDefinedName objects that have broken refs
      #>
     [XLSXDefinedNames]findBrokenRefs(){
+
+        # Set up vars we need
         $names = [XLSXDefinedNames]::new()
+        [int]$i = 0
+        $total = $this.count
         
         foreach($item in $this){
             if($item.getValue() -like '*#REF!*'){
                 $names.add($item)
             }
+
+            # Since this can run slow, give a progress bar
+            $i++
+            $pct = [math]::Round((($i / $total)*100),2)
+            Write-Progress -Activity "Finding broken references" -Status "$pct% Complete" -PercentComplete $pct
         }
 
+        Write-Progress -Activity "Finding broken references" -Completed
         return $names
     }
 
@@ -44,11 +54,18 @@ class XLSXDefinedNames : System.Collections.Generic.List[PSObject]{
      #
      # NOTE: This is desctructive and will remove data!
      #
+     # NOTE: This may take some time to run on large datasets
+     #
      # @param XLSXDefinedNames $names Takes a XLSXDefinedNames list of XLSXDefinedName objects to remove from this object
      # @return int Number of items removed from this object
      #>
     [int]removeNamesByObjects([XLSXDefinedNames]$names){
+
+        # Set up vars we need
         [int]$removed = 0
+        [int]$i = 0
+        $total = $names.count
+
         foreach($item in $names){
             if($this.Remove($item)){
                 $removed++
@@ -57,7 +74,14 @@ class XLSXDefinedNames : System.Collections.Generic.List[PSObject]{
                 $str = "Failed to remove item: " + $item.getName()
                 throw $str
             }
+
+            # Since this can run slow, give a progress bar
+            $i++
+            $pct = [math]::Round((($i / $total)*100),2)
+            Write-Progress -Activity "Removing defined names" -Status "$pct% Complete: $i of $total removed" -PercentComplete $pct
         }
+
+        Write-Progress -Activity "Removing defined names" -Completed
         return $removed
     }
 
