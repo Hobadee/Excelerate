0ad4627be3760a0a5f5bc9b0c337457ef22b5694
Eric Kincl <eric@kincl.net>
2022-04-10 00:09:22 -0700

    Broke method into separate sub-methods
    Also added progress bars since this will take some time once checkURI is implemented.

--- Classes/02-XLSX/XLSXExternalLinks.ps1
+++ Classes/02-XLSX/XLSXExternalLinks.ps1
@@ -19,6 +19,74 @@ class XLSXExternalLinks : System.Collections.Generic.List[PSObject]{
     }
 
 
+
+    <##
+     # Find invalid links in this object and return a new object containing those broken link objects
+     #
+     # NOTE: This may take quite some time to run if `testURI()` hasn't been run on the children yet.
+     #
+     # @return XLSXExternalLinks Returns a NEW XLSXExternalLinks object filled with broken link objects
+     #>
+    [XLSXExternalLinks]findBrokenLinks(){
+
+        # Set up vars we need
+        $links = [XLSXExternalLinks]::new()
+        [int]$i = 0
+        $total = $this.count
+        
+        foreach($item in $this){
+            if($item.testURI() -eq $false){
+                $links.add($item)
+            }
+
+            # Since this can run slow, give a progress bar
+            $i++
+            $pct = [math]::Round((($i / $total)*100),2)
+            Write-Progress -Activity "Finding broken links" -Status "$pct% Complete" -PercentComplete $pct
+        }
+
+        Write-Progress -Activity "Finding broken links" -Completed
+        return $links
+    }
+
+
+    <##
+     # Removes links from this XLSXExternalLinks object by passing them as a new XLSXExternalLinks object
+     #
+     # NOTE: This is desctructive and will remove data!
+     #
+     # NOTE: This may take some time to run on large datasets
+     #
+     # @param XLSXExternalLinks $names Takes a XLSXExternalLinks list of XLSXExternalLink objects to remove from this object
+     # @return int Number of items removed from this object
+     #>
+    [int]removeLinksByObjects([XLSXExternalLinks]$links){
+
+        # Set up vars we need
+        [int]$removed = 0
+        [int]$i = 0
+        $total = $links.count
+
+        foreach($item in $links){
+            if($this.Remove($item)){
+                $removed++
+            }
+            else{
+                $str = "Failed to remove item: " + $item.getName()
+                throw $str
+            }
+
+            # Since this can run slow, give a progress bar
+            $i++
+            $pct = [math]::Round((($i / $total)*100),2)
+            Write-Progress -Activity "Removing links" -Status "$pct% Complete: $i of $total removed" -PercentComplete $pct
+        }
+
+        Write-Progress -Activity "Removing links" -Completed
+        return $removed
+    }
+
+
     <##
      # Removes *NON WORKING* links from this object
      # 
@@ -30,20 +98,9 @@ class XLSXExternalLinks : System.Collections.Generic.List[PSObject]{
      # @return int Returns number of links removed
      #>
     [int]removeBrokenLinks(){
-        [int]$removed = 0
-        
-        foreach($XLSXExternalLink in $this){
-            if($XLSXExternalLink.testURI() -eq $false){
-                if($this.Remove($XLSXExternalLink)){
-                    $removed++
-                }
-                else{
-                    $str = "Failed to remove item: " + $XLSXExternalLink.getRID()
-                    throw $str
-                }
-            }
-        }
-        return $this
+        $links = $this.findBrokenLinks()
+        $removed = $this.removeLinksByObjects($links)
+        return $removed
     }
 
 
